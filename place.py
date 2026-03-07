01287e2b4937ad6db6e632844b2122cfc7352b6a
Nattanan-Nestle <Nattanan.k@kkumail.com>
2026-03-08 02:57:17 +0700

    add place.py

--- place.py
+++ place.py
@@ -10,10 +10,18 @@ time.sleep(5)
 print("สั่ง gripper กางออก...")
 mc.set_gripper_state(0, 100)   # 0 = เปิด
 time.sleep(3)
+print("สั่ง gripper หนีบ...")
+mc.set_gripper_state(1, 100)   # 0 = เปิด
+time.sleep(3)
 current = mc.get_coords()
 if current:
     print(f"ตำแหน่งปัจจุบัน: x={current[0]:.1f}, y={current[1]:.1f}, z={current[2]:.1f}")
 
 print("ลด Z ลงไปวาง...")
-mc.send_coords([257.6, -60.4, 120.0, 177.34, 3.57, -45.28], SPEED)
-time.sleep(4)
+mc.send_coords([257.6, -60.4, 300.0, 177.34, 0.57, -45.28], SPEED)
+time.sleep(3)
+mc.send_coords([257.6, -60.4, 98.0, 177.34, 0.57, -45.28], SPEED)
+time.sleep(3)
+print("สั่ง gripper กางออก...")
+mc.set_gripper_state(0, 100)   # 0 = เปิด
+time.sleep(3)
