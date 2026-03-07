9302c53befa72f189a8f7bcac7aeb44cbf29027b
Nattanan-Nestle <Nattanan.k@kkumail.com>
2026-03-08 02:51:41 +0700

    add place.py

---
+++ place.py
@@ -0,0 +1,19 @@
+from pymycobot.mycobot280 import MyCobot280
+import time
+
+SPEED = 20
+
+mc = MyCobot280("/dev/ttyTHS1", 1000000)
+
+mc.send_angles([0, 45, -120, -13, 0, -45], SPEED)
+time.sleep(5)
+print("สั่ง gripper กางออก...")
+mc.set_gripper_state(0, 100)   # 0 = เปิด
+time.sleep(3)
+current = mc.get_coords()
+if current:
+    print(f"ตำแหน่งปัจจุบัน: x={current[0]:.1f}, y={current[1]:.1f}, z={current[2]:.1f}")
+
+print("ลด Z ลงไปวาง...")
+mc.send_coords([257.6, -60.4, 120.0, 177.34, 3.57, -45.28], SPEED)
+time.sleep(4)
